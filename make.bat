f6aa787fcdf3d8c0ec8ec018d0dedfef96caa70f
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-08-16 14:38:53 +0200

    test: latex files

---
+++ make.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+
+pushd %~dp0
+
+SET BUILDDIR=out
+
+if "%1" == "pdf" goto pdf
+if "%1" == "complex-pdf" goto complex-pdf
+
+:pdf
+cd src/resume
+pdflatex -output-directory=%BUILDDIR% revathy-simple-resume.tex --interaction=nonstopmode
+if NOT EXIST %BUILDDIR%/test.pdf (
+Echo "no pdf generated!"
+exit /b 1)
+Echo "pdf generated!"
+goto end
+
+:complex-pdf
+cd src/resume
+pdflatex -output-directory=%BUILDDIR% revathy-venugopal.tex --interaction=nonstopmode
+if NOT EXIST %BUILDDIR%/revathy-venugopal.pdf (
+Echo "no pdf generated!"
+exit /b 1)
+Echo "pdf generated!"
+goto end
+
+
+:end
+popd
